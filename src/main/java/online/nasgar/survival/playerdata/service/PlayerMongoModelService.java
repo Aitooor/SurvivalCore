3358e3b200fe7e226a30d9f41a9ddfd1f0ae4aa4
cassha <notcacha1998@gmail.com>
2022-02-24 22:35:00 -0300

    refact: wrapped the not exists user to create

--- src/main/java/online/nasgar/survival/playerdata/service/PlayerMongoModelService.java
+++ src/main/java/online/nasgar/survival/playerdata/service/PlayerMongoModelService.java
@@ -6,12 +6,27 @@ import net.cosmogrp.storage.model.meta.ModelMeta;
 import net.cosmogrp.storage.mongo.MongoModelService;
 import online.nasgar.survival.playerdata.PlayerData;
 import online.nasgar.survival.playerdata.parser.PlayerMongoModelParser;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.UUID;
 import java.util.concurrent.Executor;
 
 public class PlayerMongoModelService extends MongoModelService<PlayerData> {
 
     public PlayerMongoModelService(Executor executor, MongoDatabase database, ModelService<PlayerData> cacheModelService) {
         super(executor, new ModelMeta<>(PlayerData.class).addProperty("collection", "users"), cacheModelService, database, new PlayerMongoModelParser());
+    }
+
+    @Override
+    public @Nullable PlayerData getOrFindSync(String id) {
+        PlayerData playerData = super.getOrFindSync(id);
+
+        if (playerData == null) {
+            playerData = new PlayerData(
+                    UUID.fromString(id)
+            );
+        }
+
+        return playerData;
     }
 }
