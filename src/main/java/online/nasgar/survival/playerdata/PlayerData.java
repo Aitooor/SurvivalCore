0aa0d6a4182888601aff43ae84f190bdf5746669
cassha <notcacha1998@gmail.com>
2022-03-11 15:24:26 -0300

    fix: added the null handle to itemStackArrayToBase64

--- src/main/java/online/nasgar/survival/playerdata/PlayerData.java
+++ src/main/java/online/nasgar/survival/playerdata/PlayerData.java
@@ -12,7 +12,6 @@ import org.bson.Document;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 
 import java.util.ArrayList;
