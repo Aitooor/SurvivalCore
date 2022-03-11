6b9895a0079e1b41bc38c5ecf840c61185c99a62
cassha <notcacha1998@gmail.com>
2022-03-11 15:11:34 -0300

    deps: remove TimedRankup.jar and added the local dependency

--- src/main/java/online/nasgar/survival/playerdata/PlayerData.java
+++ src/main/java/online/nasgar/survival/playerdata/PlayerData.java
@@ -12,6 +12,7 @@ import org.bson.Document;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 
 import java.util.ArrayList;
